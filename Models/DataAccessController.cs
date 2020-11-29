825574bd72796e741f523fd25608bed93fedc3e5
Tom Reid <Treid.nz@gmail.com>
2020-11-29 21:06:19 +1300

    remove code

--- Models/DataAccessController.cs
+++ Models/DataAccessController.cs
@@ -10,33 +10,27 @@ namespace PaymentUsersApp.Models
 {
     public class DataAccessController
     {
-        // Add your connection string in the following statements
-        string connectionString = "Server=payment-server-demo.postgres.database.azure.com;Database=paymentapp;Port=5432;User Id=paymentadmin@payment-server-demo;Password=Paymentapp69;Ssl Mode=Require;";
+        // TODO: Add your connection string in the following statements
+        string connectionString = "<Azure Database for PostgreSQL Connection String>";
 
         // Retrieve all details of courses and their modules    
         public IEnumerable<Users> GetAllUsers()
         {
             List<Users> userList = new List<Users>();
 
-            // Connect to the database
-            using (var conn = new NpgsqlConnection(connectionString))
+            // TODO: Connect to the database
+            //using ()
 
             {
                 Console.Out.WriteLine("Opening connection");
-                conn.Open();
-                using (var command = new NpgsqlCommand("SELECT * FROM users", conn))
-                {
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        string userID = reader.GetInt32(0).ToString();
-                        string userName = reader.GetString(1);
-                        int moduleSequence = reader.GetInt32(2);
-                        Users user = new Users(userID, userName, moduleSequence);
-                        userList.Add(user);
-                    }
-                }
-                conn.Close();
+
+                // TODO: Specify the SQL query to run
+
+                // TODO: Execute the query
+
+                // TODO: Read the data a row at a time
+            
+                // TODO: Close the database connection
             }
             return userList;
         }
